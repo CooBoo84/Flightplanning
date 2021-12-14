10b8b865ba3b054de83d905f995a0abeb083b03c
Gurra <g.linnar@gmail.com>
2021-12-14 14:24:00 +0100

    Third attempt to fix merge conlfict

--- src/main/java/se/iths/flightplanning/view/MvcConfig.java
+++ src/main/java/se/iths/flightplanning/view/MvcConfig.java
@@ -10,20 +10,11 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-<<<<<<< HEAD
         registry.addViewController("/application").setViewName("application");
         registry.addViewController("/admin").setViewName("admin");
         registry.addViewController("/login").setViewName("login");
 
     }
 
-=======
-        registry.addViewController("/users").setViewName("users");
-        registry.addViewController("/routes").setViewName("routes");
-        registry.addViewController("/admin").setViewName("admin");
-        registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/register").setViewName("register");
 
-    }
->>>>>>> d705337efabc22873f15df12bfde23dfd2219f3f
 }
