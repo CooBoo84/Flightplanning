b59543e4a19538951ed0cd3f8a0860600ba8eace
Gurra <g.linnar@gmail.com>
2021-12-16 14:47:10 +0100

    Refactored Route Controllern

--- src/main/java/se/iths/flightplanning/controller/RouteController.java
+++ src/main/java/se/iths/flightplanning/controller/RouteController.java
@@ -4,27 +4,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import se.iths.flightplanning.dto.RouteDto;
-import se.iths.flightplanning.service.RouteService;
+import se.iths.flightplanning.service.RouteServiceDto;
 
 @RestController
 @RequestMapping("routes")
 public class RouteController {
 
-    private final RouteService routeService;
+    private final RouteServiceDto routeServiceDto;
 
-    public RouteController(RouteService routeService) {
-        this.routeService = routeService;
+    public RouteController(RouteServiceDto routeServiceDto) {
+        this.routeServiceDto = routeServiceDto;
     }
 
     @PostMapping()
     public ResponseEntity<RouteDto> createRoute(@RequestBody RouteDto routeDto) {
-        RouteDto createdRoute = routeService.createRoute(routeDto);
+        RouteDto createdRoute = routeServiceDto.createRoute(routeDto);
         return new ResponseEntity<>(createdRoute, HttpStatus.CREATED);
     }
 
     @GetMapping()
     public ResponseEntity<Iterable<RouteDto>> findAllRoutes() {
-        Iterable<RouteDto> allRoutes = routeService.findAllRoutes();
+        Iterable<RouteDto> allRoutes = routeServiceDto.findAllRoutes();
         return new ResponseEntity<>(allRoutes, HttpStatus.OK);
     }
 }
