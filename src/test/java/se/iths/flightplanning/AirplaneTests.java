abfb3058fb18fb68be775a4c362cc1b365d12d27
Hans Sandblom <hans.sandblom@iths.se>
2022-01-15 10:45:58 +0100

    test: added delete test to AirplaneTests

--- src/test/java/se/iths/flightplanning/AirplaneTests.java
+++ src/test/java/se/iths/flightplanning/AirplaneTests.java
@@ -1,16 +1,15 @@
 package se.iths.flightplanning;
 
 import com.google.gson.Gson;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,7 +19,6 @@ import se.iths.flightplanning.controller.AirplaneController;
 import se.iths.flightplanning.dto.AirplaneDto;
 import se.iths.flightplanning.service.AirplaneServiceDto;
 
-import java.text.ParseException;
 import java.util.List;
 import java.util.Optional;
 
@@ -91,7 +89,13 @@ public class AirplaneTests extends WebSecurityConfigurerAdapter {
                         .accept(MediaType.APPLICATION_JSON))
                 .andReturn();
         assertThat(result.getResponse().getStatus()).isEqualTo(200);
+    }
 
+    @Test
+    void testDeleteAirplaneSuccess() throws Exception {
+        new AirplaneDto("Model-Z", 333, 33);
+        airplaneServiceDto.deleteById(1L);
+        Mockito.verify(airplaneServiceDto).deleteById(1L);
     }
 
 }
